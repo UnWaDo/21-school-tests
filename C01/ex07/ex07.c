8f81cc8507486b0096558c8d72c6eaed0de5f049
Leontine Alex <lalex@ox-q5.msk.21-school.ru>
2021-06-11 23:22:29 +0300

    Modified test for C01ex07

--- C01/ex07/ex07.c
+++ C01/ex07/ex07.c
@@ -5,23 +5,38 @@ void	ft_rev_int_tab(int *tab, int size);
 int	main (void)
 {
 	int		a[6];
+	int		b[7];
 	int		s;
 	int 	i;
 
 	s = 6;
 	i = 0;
-	printf("Was: ");
+	printf("Was:\t");
 	while (i < s)
 	{
 		a[i] = i;
 		printf("%d", a[i]);
 		i++;
 	}
-	printf("\nNow: ");
+	i = 0;
+	printf("\n\t");
+	while (i < s + 1)
+	{
+		b[i] = i;
+		printf("%d", b[i]);
+		i++;
+	}
+
+	printf("\nNow:\t");
 	ft_rev_int_tab(a, s);
+	ft_rev_int_tab(b, s + 1);
 	i = -1;
 	while (++i < s)
 		printf("%d", a[i]);
+	i = -1;
+	printf("\n\t");
+	while (++i < s + 1)
+		printf("%d", b[i]);
 	printf("\n");
 	return (0);
 }
